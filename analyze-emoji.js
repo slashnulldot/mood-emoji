b354322589b8978033e086386aa131604dbfb6ed
D:/Another/Git/null <nullnull.dot@gmail.com>
2021-10-17 15:17:19 +0300

    ✨🔖 Release of the working version

--- analyze-emoji.js
+++ analyze-emoji.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const fileContents = fs.readFileSync('mood-dict.json');
+const emojiToMood = JSON.parse(fileContents);
 
+// 0 - path to node, 1 - path to "analyze-emoji.js", [2:] - args to analyze
+const userInput = [... process.argv[2].replace(/[а-яa-z\d\s.,?!]/gi, '') ];
+
+let textMood = 0;
+let counter = 0;
+
+for(const emoji in userInput) {
+  const emojiMood = emojiToMood[userInput[emoji]];
+
+  if(emojiMood) {
+    textMood += emojiMood;
+    counter++; 
+  }
+}
+// Ternary operation for cases without emojis in text
+console.log("Настроение сообщения: " + (textMood/counter ? textMood/counter : 0));
