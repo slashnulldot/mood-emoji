2f507675753fc47c01c2ac6881225cd975b22971
jorseey <92591849+jorseey@users.noreply.github.com>
2021-10-27 20:32:40 +0300

    🔧🚧Changes in code

--- analyze-emoji.js
+++ analyze-emoji.js
@@ -1,14 +1,20 @@
 const fs = require('fs');
+
 const fileContents = fs.readFileSync('mood-dict.json');
 const emojiToMood = JSON.parse(fileContents);
 
 // 0 - path to node, 1 - path to "analyze-emoji.js", [2:] - args to analyze
-const userInput = [... process.argv[2].replace(/[а-яa-z\d\s.,?!]/gi, '') ];
+
+const userInput = [...process.argv[2]];
+
+for (const char of userInput) {
+  ...
+}
 
 let textMood = 0;
 let counter = 0;
 
-for(const emoji in userInput) {
+for(const emoji of userInput) {
   const emojiMood = emojiToMood[userInput[emoji]];
 
   if(emojiMood) {
@@ -17,4 +23,4 @@ for(const emoji in userInput) {
   }
 }
 // Ternary operation for cases without emojis in text
-console.log("Настроение сообщения: " + (textMood/counter ? textMood/counter : 0));
+console.log("Настроение сообщения: " + (counter ? textMood/counter : 0));
